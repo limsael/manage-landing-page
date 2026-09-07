ae2b5be502c2eb960d0f3edfd3c63a1226a8586e
limsael <salimtoure525@gmail.com>
2026-09-07 08:24:01 +0000

    Nav List toggle for mobile devices

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const navList = document.getElementById("header-nav-list");
+const navToggle = document.getElementById("header-nav-toggle");
+const navToggleImg = document.querySelector("#header-nav-toggle img");
+const navLinks = document.querySelectorAll(".header__nav-link");
+
+/* ================ Functions ============== */
+
+function toggleNavbar() {
+  navList.classList.toggle("active");
+
+  if (navList.classList.contains("active")) {
+    navToggleImg.src = "./assets/images/icon-close.svg";
+  } else {
+    navToggleImg.src = "./assets/images/icon-hamburger.svg";
+  }
+}
+
+function closeNavbar() {
+  navList.classList.remove("active");
+  navToggleImg.src = "./assets/images/icon-hamburger.svg";
+}
+
+/* ================ Events ============== */
+
+navToggle.addEventListener("click", toggleNavbar);
+navLinks.forEach((navlink) => {
+  navlink.addEventListener("click", closeNavbar);
+});
